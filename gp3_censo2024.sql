a66ade7f5f673ff3033425db9372440e97860af8
gaboardi22 <andresgaboardi@gmail.com>
2024-09-22 13:35:02 -0300

    .
    ver si carga el script

---
+++ gp3_censo2024.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-09-2024 a las 18:34:02
+-- Versión del servidor: 8.0.35
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gp3_censo2024`
+--
+CREATE DATABASE IF NOT EXISTS `gp3_censo2024` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `gp3_censo2024`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habitante`
+--
+
+CREATE TABLE `habitante` (
+  `nro_habitante` int NOT NULL,
+  `provincia` varchar(50) NOT NULL,
+  `edad` int NOT NULL,
+  `nivel_educativo` varchar(50) NOT NULL,
+  `usa_computadora` tinyint(1) NOT NULL,
+  `trabaja` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `habitante`
+--
+ALTER TABLE `habitante`
+  ADD PRIMARY KEY (`nro_habitante`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `habitante`
+--
+ALTER TABLE `habitante`
+  MODIFY `nro_habitante` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
